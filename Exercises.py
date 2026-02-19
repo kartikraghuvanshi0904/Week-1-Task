8f06cb1501afab543c500989e3dfa79a878df43b
kartikraghuvanshi0904 <kartikraghuvanshi0904@gmail.com>
2026-02-19 18:06:52 +0530

    Add files via upload

---
+++ Exercises.py
@@ -0,0 +1,718 @@
+'''
+password = int(input("Enter the Password"))
+if(password == 123):
+	radius = int(input("Enter the radius"))
+	area = 3.14*radius*radius
+	print("Area of Circle", area)
+else:
+	print("Invalid Password")
+
+
+password = int(input("Enter Password"))
+if (password==123):
+	base= int(input("Enter the base"))
+	height= int(input("Enter the height"))
+	area=0.5*base*height
+	print("Area of Triangle is:", area)
+else:
+	print("Invalid Password")
+
+password = int(input("Enetr Password"))
+if(password==123):    
+	length = int(input("Enter the length"))
+	base = int(input("Enter the Base"))
+	area = length*base
+	print("The area of Rectangle is:", area)
+else:
+	print("Invalid Password")   
+
+password = int(input("Enter the password"))
+if(password== 1234):
+	side =int(input("Enter the side of square"))
+	area= side*side
+	print("The Area of Square is",area)
+else:
+	print("Invalid Password")
+
+
+password=  int(input("Enter the Password"))
+if(password==123):
+	principle=int(input("Enter the Principle amount"))
+	rate= int(input("Enter the Rate of Intrest"))
+	time=int(input("Enter the Time period"))
+	si=(principle*rate*time)/100
+	print("Simple Intrest is Equal to :",si)
+else:
+	print("Invalid Pasword")
+
+
+
+import subprocess 
+password=input("Enter the Password")
+if(password=="ABC"):
+	radius = int(input("Enter the radius"))
+	area = 3.14*radius*radius 
+	print("Area of Circle", area)
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+import subprocess
+password=input("Enter the PAssword")
+if(password=="ABC"):
+	radius = int(input("Enter the radius"))
+	area = 3.14*radius*radius
+	print("Area of Circle", area)
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+
+
+
+import subprocess 
+password=input("Enter the password")
+if(password=="ABC"):
+	radius=int(input("Enter the Radius"))
+	area=3.14*radius*radius
+	print("Area of Circle is",area)
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+
+
+import subprocess 
+password=input("Enter the password")
+if(password=="ABC"):
+	print("hello user")
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+
+
+import subprocess
+password=input("Entger the password")
+if(password=="ABC"):
+	print("Hello world")
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+
+
+import subprocess
+password=input("Enter password")
+if(password=="ABC"):
+	print("Hello")
+else:
+	command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+ 
+
+import subprocess 
+passsword = input('Enter the Password")
+if (password=="ABC"):
+	print("Hello World")
+else:
+	command = "c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+	subprocess.call(command)
+
+import subprocess
+password = Input("Enter Password")
+if(password=="ABC"):
+	print("Hello World")
+else:
+command="c:/windows/system32/rundll32.exe user32.dll,LockWorkStation"
+subprocess.call(command)
+'''
+
+
+'''
+x = 77
+y= "hello"
+print(type(x))
+print(type(y))
+
+
+x,y,z = "Apple","Banana","Mango"
+print(x)
+print(y)
+print(z)
+
+
+x=y=z="Orange"
+print(x)
+print(y)
+print(z)
+
+'''
+
+
+# fruits=["Apples", "Banana", "Orange"]
+# x,y,z=fruits
+# print(x)
+# print(y)
+# print(z)	    
+
+# marks ={
+# 	"Harry" : 100,
+#     "Shubham": 300,
+#     "Rohan":323,
+#     0 :"harry"
+# }
+# print(marks=["Harry"])
+# print(marks.keys())
+# print(marks.values())
+# print(marks.update({"Harry":99}))
+# print(marks)
+# marks.update({"harry":99, "Raghav":0})
+# print(marks)
+
+# print(marks.get("Harry"))
+# print(marks["Harry"])
+# print(marks.items()) 
+"""
+
+
+s ={1,2,3,4,232,221,33,232,2321,"Hello","World"}
+print(s)
+s.add(777)
+print(s)
+print(type(s))
+print(s.remove(777))
+print(s.pop())
+
+s1 = {1,2,3,4,5,6,7}
+s2 = {6,7,8,9,10}
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+"""
+
+
+# words = {
+#     "madad" : "Help",
+#     "kursi" : "chair",
+#     "billi" : "cat"
+# }
+
+# word = input("Enter the word you need to understand")
+# print(words[word])
+
+
+
+"""
+s = set()
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+n = input("Enter a number :")
+s.add(int(n))
+print(s)
+
+s = set()
+s.add(18)
+s.add("18")
+print(s)
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+print(s)
+
+s = {}
+print(type(s))
+
+"""
+
+
+# d = {}
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# d.update({name: lang})
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# d.update({name: lang})
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# d.update({name: lang})
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# d.update({name: lang})
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# d.update({name: lang})
+# name = input("Enter friend's name: ")
+# lang = input("Enter Language name: ")
+
+# print(d)
+
+# age = int(input("Enter the Age of the user"))
+# if(age>=18):
+#     print("Yes")
+
+
+
+# a1 = int(input("Enter number 1 : "))
+# a2 = int(input("Enter number 2 : "))
+# a3 = int(input("Enter number 3 : "))
+# a4 = int(input("Enter number 4 : "))
+
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("Greatest Number is a1:", a1)
+    
+
+# if(a2>a1 and a2>a3 and a2>a4):
+#     print("Greatest Number is a2:", a2)
+    
+
+# if(a3>a1 and a3>a2 and a3>a4):
+#     print("Greatest Number is a3:", a3)
+# if(a4>a1 and a4>a2 and a4>a3):
+#     print("Greatest Number is a4:", a4)
+    
+
+"""
+marks1 =int(input("Enter Marks 1 : "))
+marks2 =int(input("Enter Marks 2 : "))
+marks3 =int(input("Enter Marks 3 : "))
+
+total_percentage = ((marks1+marks2+marks3)*100)/300
+
+
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("Congratulations You have passed the Exam",total_percentage)
+else:
+    print("You have failed the Exam Try again next year", total_percentage)
+    """
+
+# p1 = "Make a lot of Money"
+# p2 = "buy now"
+# p3 = "subscribe now"
+# p4 = "check this"
+
+# message = input("enter your comment: ")
+# if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+#     print("This Comment is a Spam")
+
+# else:
+#     print("This comment is not a Spam")
+    
+
+"""
+username = input("Enter your user Name")
+if(len(username)<10):
+    print("Your USer Name contains less than 10 Character")
+else:
+    print("Your User Name cocntains more than or equal to 10 characters")   
+ """
+
+
+# l = ["Kartik", "Swapndeep", "Raghav", "Akshat"]
+# name = input("Enter your name")
+# if(name in l):
+#     print("Your name",name, "is in the list")
+# else:
+#     print("Your name",name,"is not in the list")
+
+"""
+
+marks = int(input("Enter your Marks: "))
+
+if((marks<=100 and marks>=90)):
+    grade = "Excellent"
+    
+elif((marks<=90 and marks>=80)):
+    grade = "A"
+    
+
+elif((marks<=80 and marks>=70)):
+    grade = "B"
+    
+elif((marks<=70 and marks>=60)):
+    grade = "C"
+    
+elif((marks<=60 and marks>=50)):
+    grade = "D"
+    
+elif(marks<50):
+    grade = "Fail"
+    
+
+print('Your Grade is: ',grade)
+
+"""
+
+# post = input("enter the Post: ")
+
+# if("harry" in post.lower()):
+#     print("This post is talking about Harry")
+
+# else:
+#     print("This post is not talking about Harry")
+
+
+
+"""
+for i in range(1,6):
+    print(i)
+    
+
+i = 1
+while(i<51):
+    print(i)
+    i+=1
+    
+i = 0
+while(i<5):
+    print("Hello")
+    i = i+1
+    
+
+l = ["Hello","World","dd","edwdw","f33ef3e"]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i = i+1
+    
+for i in range(0,55,5):
+    print(i)
+
+l=[31,423,42,34,324,52,3,4]
+
+for item in l:
+    print(item)
+else:
+    print("Done")
+    
+	"""
+
+# for i in range(100):
+#     if(i==50):
+#         break
+#     print(i)
+    
+# for i in range(100):
+#     if(i==50):
+#         continue
+#     print(i)	
+    
+"""
+n = int(input("Enter a Number: "))
+for i in range(1,11):
+    print(n, "x" ,i ,"=",n*i)	
+    
+l =["Harry","Soham","Sachin","fufa","kalu","Gujji"]
+
+for name in l:
+    if(name.startswith("S")):
+        print("Hello",name)
+        
+n = int(input("Enter a Number:"))
+
+i=1
+while(i<11):
+    print(n,"x",i,"=",n*i)
+    i =i+1
+    
+    """
+
+# n = int(input("Enter a Number : "))
+
+# for i in range(2,n):
+#     if(n%i)==0:
+#         print("Number is not Prime")
+#         break
+# else:
+#     print("Number is Prime")
+    
+
+# n = int(input("Enter a Number"))
+# i=0
+# sum=0
+# while(i<=n):
+#     sum=sum+i
+#     i=i+1
+    
+# print(sum)	
+           
+        
+# n = int(input("Enter a NUmber: "))
+# product = 1
+# for i in range(1,n+1):
+#     product = product*i
+    
+# print(f"The Factorial of{n} is{product}")
+ 
+# n = int(input("Enter a Number: "))
+# for i in range(1,n+1):
+#     print(" "*(n-i),end="")
+#     print("*"* (2*i-1),end="")
+#     print() 
+    
+"""
+n = int(input("Enter the Number: "))
+for i in range(1, n+1):
+    if(i==1 or i==n):
+        print("*"*n,end="")
+    else:
+        print("*",end="")
+        print(" "*(n-2),end="")
+        print("*",end="")
+    print("")
+   
+    
+    
+n= int(input("Enter the Number:"))
+for i in range(1,11):
+    print(f"{n} X {i} = {n*(i)}")
+
+    
+#     """
+# def avg(): # Function Defination
+    
+#     a = int(input("Enter a Number"))
+#     b = int(input("Enter a Number"))
+#     c = int(input("Enter a Number"))
+#     average = (a + b + c)/3
+#     print(average)
+# avg()  # Function call 
+ 
+# def factorial(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return n*factorial(n-1)
+# n= int(input("Enter a Number :")) 
+# print("The Factorial of this Number is",factorial(n))
+
+
+# def greatest():
+#     a=int(input("Enter a Number"))
+#     b=int(input("Enter a Number"))
+#     c=int(input("Enter a Number"))
+#     if(a==b==b==c):
+#         return (f"Values are Equal")
+#     elif(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c
+# print(greatest())    
+
+# def f_to_c(f):
+#     return 5*(f-32)/9
+
+# f = int(input("ENTER temperature in F: "))
+# c = f_to_c(f)
+# print(f"{round(c, 2)}C")
+
+
+# def goodDay(name,ending):
+#     print("Good Day "+name,ending)
+#     return "as"  
+
+# a = goodDay("Harry","Thankyou")
+# print(a)
+
+"""
+def goodDay(name,ending="Thank You"):
+    print("Good Day",name)
+    print(ending)
+    
+goodDay("Harry", "Thanks")
+goodDay("Raghav")
+
+"""
+
+# def square(number):
+#     return number ** 2
+
+# result = square(5)
+# print(result)
+
+
+
+# def factorial(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return n*factorial(n-1)
+# n= int(input("Enter a Number: "))
+# print("the factorial of the Number",n,"is :",factorial(n))
+"""
+def greatest():
+    a = int(input("Enter first Number: "))
+    b = int(input("Enter second Number: "))
+    c = int(input("Enter third Number: "))
+    if(a==b==c or a==b or a==c):
+        return "Numbers are same to predict the Greatest one"   
+    elif(a>b and a>c):
+        return (a,"is Greatest")
+    elif(b>a and b>c):
+        return(b,"is Greatest")
+    elif(c>a and c>b):
+         return(c,"is Greatest")
+print(greatest())    
+  """
+
+# def temp(f):
+#     return( 5*(f-32)/9)
+# f=int(input("Enter Temperature in F"))
+# print(temp(f),"Degree Celcius")
+
+
+# def conversion(inch):
+#     return inch*2.54
+# n = int(input("Enter a Number:"))
+# print("The value in centimeters is: ",conversion(n))
+
+
+
+# computer = -1
+# you = =input("Enter your choice: ")
+# youDict
+
+
+# import webbrowser
+# webbrowser.open("https://www.youtube.com/watch?v=UrsmFxEIp5k")
+# import calendar
+# import datetime
+# print(calendar.calendar(2020,1))
+# print(datetime.datetime.now(  ))
+
+
+# FILE I/O
+
+# f = open("D:\Python Practice/New.py")
+# data = f.read()
+# print(data)
+# f.close()
+
+# st = "Hello World this is my first program"
+# f = open("myfile.txt","w")
+# f.write(st)
+# print("File Written successfully")
+# f.close()
+
+
+# f = open("myfile.txt")
+
+# lines = f.readlines()
+
+# print(lines, type(lines))
+
+# f.close()
+
+
+# f = open("myfile.txt")
+
+# line1 = f.readline()
+# print(line1,type(line1))
+
+# line2 = f.readline()
+# print(line2,type(line2))
+
+# line3 = f.readline()
+# print(line3,type(line3))
+
+# line4 = f.readline()
+# print(line4,type(line4))
+
+# line5 = f.readline()
+# print(line5,type(line5))
+
+# f.close()
+
+
+# st = "Hello World my first program"
+
+# f = open("myfile.txt","a")
+
+# f.write(st)
+# f.close()
+
+
+# OOPS 
+
+# class Employee:
+#     name = "Harry"
+#     language = "python"
+#     salary = 1234567890
+
+# harry = Employee()
+# harry.name = "Harry"
+# print(harry.name,harry.language,harry.salary)    
+
+# rohan = Employee()
+# rohan.name = "Rohan"
+# print(rohan.name,rohan.language,rohan.salary)
+
+
+# class Employee:
+#     name = "Harry" # class Attributes
+#     language = "python"
+#     salary = 1234567890
+
+# harry = Employee()
+# harry.name = "Harry" #instance attributess
+# harry.language = "Java"
+# print(harry.name,harry.language,harry.salary)    
+
+# #instance attributes are also priotrized above the class attributes
+	
+"""
+class programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+            self.name = name
+            self.salary = salary
+            self.pin = pin
+    
+p = programmer("Harry", 120000, 2313231)
+print(p.name, p.salary, p.pin ,p.company)
+r = programmer("Rohan",120000,2313231)
+print(r.name, r.salary, r.pin, r.company)
+ 
+
+"""
+
+
+# class calculator:
+#     def __init__(self,n):
+#         self.n = n
+#     def square(self):
+#         print(f"The square is {self.n*self.n}")
+#     def cube(self):
+#         print(f"The cube is{self.n*self.n*self.n}")
+#     def squareroot(self):
+#         print(f"The squareroot is{self.n**1/2}")
+		
+#     @staticmethod
+#     def hello():
+#          print("Hello There")
+
+# a = calculator(4)
+# a.hello()
+# a.cube()
+# a.squareroot()	
+ 
+
+
+  
